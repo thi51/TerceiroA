647356a873ca716f64f224b7ece2a9511a759817
LOGONPF <LOGONPF@L03MICRO43>
2023-10-05 13:11:56 -0300

    Add project files.

---
+++ AppTerceiroA/AppTerceiroA/Conexao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTerceiroA
+{
+    public static class Conexao
+    {
+        public static string strConexao = "server=bancomodulo.ddns.net;port=3306;user=lucas.moreira;password=1234;database=bdmodulo;";
+    }
+}
---
+++ AppTerceiroA/AppTerceiroA/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppTerceiroA
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Clicked(object sender, EventArgs e)
+        {
+            txtNome.Text = "Parabens turma, tudo certo!";
+        }
+    }
+}
