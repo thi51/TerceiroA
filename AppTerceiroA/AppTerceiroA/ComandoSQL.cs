89e1284e72f078aa0dfe455485b225c59466e4b3
LuscaFusca <lucasgomes_2006@hotmail.com>
2023-10-09 08:09:28 -0300

    Criação da classe ComandoSQL

---
+++ AppTerceiroA/AppTerceiroA/ComandoSQL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTerceiroA
+{
+    public static class ComandoSQL
+    {
+        public static string insereNome = "INSERT INTO tbl_cliente(nome) VALUES (@nome)";
+    }
+}
