f9faabad012d36898adfdcfaa423957d2fb7cbb8
thiago.feudo <thisimba51@gmail.com>
2023-10-23 08:16:18 -0300

    Coloquei o insert

--- AppTerceiroA/AppTerceiroA/ComandoSQL.cs
+++ AppTerceiroA/AppTerceiroA/ComandoSQL.cs
@@ -6,6 +6,6 @@ namespace AppTerceiroA
 {
     public static class ComandoSQL
     {
-        public static string insereNome = "INSERT INTO tbl_cliente( NOME, EMAIL, RG, SENHA, TIPO) VALUES ('João','joao@gmail.com','53.777.888-99','Lindo','cliente')";
+        public static string insereNome = "INSERT INTO tbl_cliente(ID_CLIENTE, NOME, EMAIL, RG, SENHA, TIPO) VALUES ('265.9852.658-58', 'João','joao@gmail.com','53.777.888-99','Lindo','cliente')";
     }
 }
